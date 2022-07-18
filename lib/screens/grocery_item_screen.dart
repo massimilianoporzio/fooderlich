d7c1595ac5d313e743522c890f9727ad724b7021
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-07-18 14:24:17 +0200

    aggiunta scaffold per navigare e tornare indweitro

--- lib/screens/grocery_item_screen.dart
+++ lib/screens/grocery_item_screen.dart
@@ -27,10 +27,79 @@ class GroceryItemScreen extends StatefulWidget {
 }
 
 class _GroceryItemScreenState extends State<GroceryItemScreen> {
+  final _nameController = TextEditingController();
+  String _name = '';
+  Importance _importance = Importance.low;
+  DateTime _dueDate = DateTime.now();
+  TimeOfDay _timeOfDay = TimeOfDay.now();
+  Color _currentColor = Colors.green;
+  int _currentSliderValue = 0;
+
   @override
   Widget build(BuildContext context) {
     // TODO 12: Add GroceryItemScreen Scaffold
-    return Container(color: Colors.orange);
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                //TODO 24 add callback
+              },
+              icon: Icon(Icons.check))
+        ],
+        elevation: 0.0,
+        title: Text(
+          'Grocery Item',
+          style: GoogleFonts.lato(fontWeight: FontWeight.w600),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            // TODO 13: Add name TextField
+            // TODO 14: Add Importance selection
+            // TODO 15: Add date picker
+            // TODO 16: Add time picker
+            // TODO 17: Add color picker
+            // TODO 18: Add slider
+            // TODO: 19: Add Grocery Tile
+          ],
+        ),
+      ),
+    );
+  }
+
+  // TODO: Add initState()
+  @override
+  void initState() {
+    // TODO: implement initState
+    //STATOini INIZIALE: SE originalItem non è null mostro i suo valori
+    final originalItem = widget.oringinalItem;
+    if (originalItem != null) {
+      _nameController.text = originalItem.name;
+      _importance = originalItem.importance;
+      final date = originalItem.date;
+      _dueDate = date;
+      _timeOfDay = TimeOfDay(hour: date.hour, minute: date.minute);
+      _currentColor = originalItem.color;
+      _currentSliderValue = originalItem.quantity;
+    }
+    _nameController.addListener(() {
+      setState(() {
+        _name = _nameController.text;
+      });
+    });
+
+    super.initState();
+  }
+
+  // TODO: Add dispose()
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _nameController.dispose();
+    super.dispose();
   }
 
   // TODO: Add buildNameField()
