95b25647f19221c0e52a05f7ea0596b50c6ca2e9
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-07-18 12:56:21 +0200

    grocery screen con scaffold e bottone

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,11 +1,35 @@
+import '../models/models.dart';
 import 'package:flutter/material.dart';
 import 'package:fooderlich/screens/empty_grocery_screen.dart';
+import 'package:provider/provider.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const EmptyGroceryScreen();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          //TODO 11
+          print("ADD ITEM");
+        },
+      ),
+      body: buildGroceryScreen(),
+    );
+  }
+
+  Widget buildGroceryScreen() {
+    return Consumer(builder: (context, GroceryManager manager, child) {
+      if (manager.groceryItems.isNotEmpty) {
+        //TODO 25
+        return Container(
+          color: Colors.amber,
+        );
+      } else {
+        return EmptyGroceryScreen();
+      }
+    });
   }
 }
